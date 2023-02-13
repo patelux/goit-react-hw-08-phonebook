10bdfa841506b8e9506981a03152040fe7ed96ff
Natalia Levenets <levenets.lot@gmail.com>
2023-02-13 22:06:28 +0200

    local storage settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,54 @@ import React from 'react';
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
+import storage from '../helpers/storage';
+
 import { PhonebookForm } from '../components/PhonebookForm/PhonebookForm';
 import { Filter } from '../components/Filter/Filter';
 import { ContactsList } from '../components/ContactsList/ContactsList';
 
 import css from '../components/PhonebookForm/PhonebookForm.module.css';
 
+const FRIENDS = [
+  {
+    id: 'id-1',
+    name: 'Rosie Simpson',
+    number: '459-12-56',
+  },
+  {
+    id: 'id-2',
+    name: 'Hermione Kline',
+    number: '443-89-12',
+  },
+  {
+    id: 'id-3',
+    name: 'Eden Clements',
+    number: '645-17-79',
+  },
+  {
+    id: 'id-4',
+    name: 'Annie Copeland',
+    number: '227-91-26',
+  },
+];
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = storage.load('contacts-list') ?? FRIENDS;
+    console.log(savedContacts);
+    this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      storage.save('contacts-list', contacts);
+    };
   };
 
   addContact = contact => {
@@ -33,10 +66,10 @@ export class App extends Component {
       };
     });
   };
-;
+
+  
   handleFilter = ({ target: { value } }) => {
-   this.setState({ filter: value });
-    ;
+    this.setState({ filter: value });
   };
 
   getFilteredContacts = () => {
